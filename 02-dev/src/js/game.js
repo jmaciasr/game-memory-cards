3d1bbc691c2ef7ffe9c4e307a5f9a887f34fad4b
jmacias <jmacias@b7-04.local>
2017-12-01 17:44:11 -0600

    commit dicember first

--- 02-dev/src/js/game.js
+++ 02-dev/src/js/game.js
@@ -1,8 +1,9 @@
-var cards = document.getElementById('cards')
-var deck = ['c1-mobile.png', 'c2-mobile.png', 'c3-mobile.png', 'c4-mobile.png', 'c5-mobile.png', 'c6-mobile.png', 'c7-mobile.png', 'c8-mobile.png', 'c9-mobile.png', 'c10-mobile.png']
-var fulldeck
-var randomDeck
-var card
+let cards = document.getElementById('cards')
+let deck = ['c1-mobile.png', 'c2-mobile.png', 'c3-mobile.png', 'c4-mobile.png', 'c5-mobile.png', 'c6-mobile.png', 'c7-mobile.png', 'c8-mobile.png', 'c9-mobile.png', 'c10-mobile.png']
+let fulldeck
+let randomDeck
+let card
+let backCard
 
 function doubleDeck () {
   fulldeck = deck.concat(deck)
@@ -19,17 +20,45 @@ function stirDeck () {
 stirDeck()
 
 function printCards () {
-  for (var i = 0; i < randomDeck.length; i++) {
+  for (let i = 0; i < randomDeck.length; i++) {
+    backCard = document.createElement('div')
+    backCard.classList.add('back-card')
     card = document.createElement('img')
     card.src = 'img/' + randomDeck[i]
-    cards.appendChild(card)
+    card.classList.add('card')
+    backCard.appendChild(card)
+    cards.appendChild(backCard)
   }
 }
 
 printCards()
 
 function hiddenCards () {
-  console.log('yeih')
+  let hiddenCards = document.getElementsByClassName('card')
+  for (let i = 0; i < hiddenCards.length; i++) {
+    hiddenCards[i].classList.add('card--hidden')
+    hiddenCards[i].setAttribute('id', 'card' + i)
+  }
 }
 
-window.setTimeout(hiddenCards, 100)
+window.setTimeout(hiddenCards, 1000)
+
+let clickCard1
+let clickCard2
+let accountant = 0
+
+function showCard (element) {
+  let currentEle = element.target
+  let currentCard = currentEle.childNodes
+  console.log(currentCard[0].id)
+  currentCard[0].classList.remove('card--hidden')
+
+  if (accountant === 0) {
+    clickCard1 = currentCard[0]
+    accountant = 1
+  } else if (accountant === 1) {
+    clickCard2 = currentCard[0]
+  }
+}
+
+document.addEventListener('click', showCard)
