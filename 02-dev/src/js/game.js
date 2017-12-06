fb9a57fe5f60300cbf2259363151c87ed1c9d5ea
jmacias <jmacias@b7-04.local>
2017-12-05 19:28:50 -0600

    all funcionality less substract the score by fail

--- 02-dev/src/js/game.js
+++ 02-dev/src/js/game.js
@@ -1,4 +1,5 @@
 let cards = document.getElementById('cards')
+let currentScore = document.getElementById('currentScore')
 let deck = ['c1-mobile.png', 'c2-mobile.png', 'c3-mobile.png', 'c4-mobile.png', 'c5-mobile.png', 'c6-mobile.png', 'c7-mobile.png', 'c8-mobile.png', 'c9-mobile.png', 'c10-mobile.png']
 let fulldeck
 let randomDeck
@@ -23,6 +24,7 @@ function printCards () {
   for (let i = 0; i < randomDeck.length; i++) {
     backCard = document.createElement('div')
     backCard.classList.add('back-card')
+    backCard.addEventListener('click', showCard)
     card = document.createElement('img')
     card.src = 'img/' + randomDeck[i]
     card.classList.add('card')
@@ -42,11 +44,13 @@ function hiddenCards () {
   }
 }
 
-window.setTimeout(hiddenCards, 1000)
+window.setTimeout(hiddenCards, 3000)
 
 let clickedCards = 0
 let previus
 let current
+let previusId
+let currentId
 let score = 0
 function showCard (element) {
   if (clickedCards === 2) {
@@ -55,22 +59,33 @@ function showCard (element) {
   let currentEle = element.target
   let currentCard = currentEle.childNodes
   if (currentEle.getAttribute('data-clicked')) {
-    alert('you need other card')
+    alert('you need click other card')
   }
   if (currentCard[0] !== undefined) {
-    currentCard[0].classList.remove('card--hidden')    
+    currentCard[0].classList.remove('card--hidden')
     currentCard[0].dataset.clicked = true
     clickedCards++
     previus = current
     current = currentCard[0].src
+    previusId = currentId
+    currentId = currentCard[0].id
   }
   if (previus === current) {
-    score =+ 100 
-    console.log(score)
-  } else if (previus !== current && clickedCards === 2) {
-    window.setTimeout(hiddenCards, 1000)
+    score = score + 100
+    currentScore.textContent = score
     clickedCards = 0
+    let id1 = document.getElementById(previusId)
+    id1.parentNode.classList.add('back-card--match')
+    id1.parentNode.removeEventListener('click', showCard, false)
+    let id2 = document.getElementById(currentId)
+    id2.parentNode.classList.add('back-card--match')
+    id2.parentNode.removeEventListener('click', showCard, false)
+    window.setTimeout(hiddenCards, 400)
+  } else if (previus !== current && clickedCards === 2) {
+    clickedCards = 0
+    current = undefined
+    previusId = undefined
+    window.setTimeout(hiddenCards, 400)
   }
 }
 
-document.addEventListener('click', showCard)
